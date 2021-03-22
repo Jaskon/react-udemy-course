55821c5a8e999a251c494157d7eadb3093fec965
Jaskon <jaskon97@gmail.com>
2021-03-22 10:52:05 +0300

    Cards are generated from an array of object

--- src/App/App.js
+++ src/App/App.js
@@ -6,7 +6,21 @@ import cl from 'classnames';
 
 const loremIpsum = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras vestibulum mauris justo, non egestas sapien malesuada sit amet.';
 
-const cards = [0, 1, 2].map(one =>
+const cards = [
+  {
+    id: '0',
+    caption: 'Card one header',
+    content: loremIpsum
+  }, {
+    id: '1',
+    caption: 'Card two header',
+    content: loremIpsum
+  }, {
+    id: '2',
+    caption: 'Card three header',
+    content: loremIpsum
+  }
+].map(one =>
   <Card
     key={one}
     caption={'Card header'}
