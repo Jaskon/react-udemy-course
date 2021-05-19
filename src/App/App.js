486270f7943295ca8f565c442ea3eed7893e16bc
Eugene Orlov <jaskon97@gmail.com>
2021-05-20 00:13:22 +0300

    Fetch card list from 'db'

--- src/App/App.js
+++ src/App/App.js
@@ -6,53 +6,6 @@ import CardList from '../CardComponents/CardList/CardList';
 import CardsContextProvider, {CardsContext} from "../CardContextComponent/CardsContextProvider";
 
 
-const loremIpsum = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras vestibulum mauris justo, non egestas sapien malesuada sit amet.';
-
-const beValues = [
-  {
-    id: '0',
-    caption: 'Card one header',
-    content: loremIpsum,
-    selected: true
-  }, {
-    id: '1',
-    caption: 'Card two header',
-    content: loremIpsum,
-    selected: true
-  }, {
-    id: '2',
-    caption: 'Card three header',
-    content: loremIpsum,
-    selected: false
-  }, {
-    id: '3',
-    caption: 'Card four header',
-    content: loremIpsum,
-    selected: false
-  }, {
-    id: '4',
-    caption: 'Card five header',
-    content: loremIpsum,
-    selected: false
-  }, {
-    id: '5',
-    caption: 'Card six header',
-    content: loremIpsum,
-    selected: false
-  }, {
-    id: '6',
-    caption: 'Card seven header',
-    content: loremIpsum,
-    selected: false
-  }, {
-    id: '7',
-    caption: 'Card eight header',
-    content: loremIpsum,
-    selected: false
-  }
-];
-
-
 function App() {
 
   const cardsContext = useContext(CardsContext);
@@ -61,7 +14,7 @@ function App() {
     <div>
       <Header
         containerStyleName={'App__container'}
-        badge={cardsContext.cards.length}
+        badge={cardsContext.cards?.length || 0}
       />
       <div className={cl('App__container', 'App__container-background')}>
         {/* Should have context with cards */}
@@ -71,5 +24,5 @@ function App() {
   );
 }
 
-export default CardsContextProvider(App, { initCardList: beValues });
+export default CardsContextProvider(App);
 export { App };
--- src/CardContextComponent/CardsContextProvider.js
+++ src/CardContextComponent/CardsContextProvider.js
@@ -1,36 +1,42 @@
 import React, { useState } from 'react';
+import {getCardList} from '../api/cards.api';
 
 
 const CardsContext = React.createContext({});
 
 
-function CardsContextProvider(Children, { initCardList }) {
+function CardsContextProvider(Children) {
 
   return function Child(props) {
 
-    const [cardsContext, setCardsContext] = useState(() => ({
-      cards: initCardList,
-      setCards: cards => setCardsContext(state => ({
-        ...state,
-        cards
-      })),
-      addCard: card => setCardsContext(state => ({
-        ...state,
-        cards: [card, ...state.cards]
-      })),
-      removeSelectedCards: () => setCardsContext(state => ({
-        ...state,
-        cards: state.cards.filter(card => !card.selected)
-      })),
-      editCard: newCard => setCardsContext(state => ({
-        ...state,
-        cards: state.cards.map(
-          one => one.id === newCard.id
-            ? {...one, ...newCard}
-            : one
-        )
-      }))
-    }));
+    const [cardsContext, setCardsContext] = useState(() => {
+      // Fetch cards from bd
+      getCardList().then(cards => cardsContext.setCards(cards));
+
+      return {
+        cards: [],
+        setCards: cards => setCardsContext(state => ({
+          ...state,
+          cards
+        })),
+        addCard: card => setCardsContext(state => ({
+          ...state,
+          cards: [card, ...state.cards]
+        })),
+        removeSelectedCards: () => setCardsContext(state => ({
+          ...state,
+          cards: state.cards.filter(card => !card.selected)
+        })),
+        editCard: newCard => setCardsContext(state => ({
+          ...state,
+          cards: state.cards.map(
+            one => one.id === newCard.id
+              ? {...one, ...newCard}
+              : one
+          )
+        }))
+      };
+    });
 
     return (
       <CardsContext.Provider value={cardsContext}>
