eced36eaa179c78c5634bd264ed48d4a95a790c4
Eugene Orlov <jaskon97@gmail.com>
2021-06-06 01:36:55 +0300

    Remove unnecessary htmlFor

--- src/Auth/Auth.js
+++ src/Auth/Auth.js
@@ -3,10 +3,10 @@ import { Link } from 'react-router-dom';
 
 function Auth() {
   return <div className={'Auth__container'}>
-    <label htmlFor="username">Username</label>
+    <label>Username</label>
     <input type="text"/>
 
-    <label htmlFor="username">Password</label>
+    <label>Password</label>
     <input type="text"/>
 
     <Link to="/">
